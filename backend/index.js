fb32204026b788a35d171e480521bde6a5300a0f
Mustafa Afaisal <mustafamfaisal@gmail.com>
2023-06-07 13:42:05 +0300

    finish the backend and start the frontend part

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-
+const axios = require("axios");
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
-});
+    const { username } = req.body;
+    // Get or create user on Chat Engine!
+    try {
+      const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        { username: username, secret: username, first_name: username },
+        { headers: { "Private-Key": "d01abf84-f960-488f-9bf2-9b659d7f2639" } }
+      );
+      return res.status(r.status).json(r.data);
+    } catch (e) {
+      return res.status(e.response.status).json(e.response.data);
+    }
+  });
 
 app.listen(3001);
